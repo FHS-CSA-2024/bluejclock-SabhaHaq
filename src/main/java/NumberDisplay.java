35ffa2320783408d4da98ea2df528bc7b7eaee35
SabhaHaq <s.h.haq826@gmail.com>
2024-10-08 21:19:38 -0700

    Update NumberDisplay.java

--- src/main/java/NumberDisplay.java
+++ src/main/java/NumberDisplay.java
@@ -1,24 +1,62 @@
 
+package src.main.java;
+
+
 
 public class NumberDisplay
 {
     //implement 2 private variables, limit and value.
     //  both should be ints
-
+    private int limit; 
+    private int value; 
+    private String displayValue; 
     
     //implement a constructor that sets the limit to a passed in variable rollOverLimit
     //  should set value to 0
+public NumberDisplay (int rollOverLimit) {
+        this.limit = rollOverLimit; 
+        this.value = 0; 
+    }
     
     //Implement getter/setter methods for limit and value
     //  The setValue method should check the input for validity
     //      (should not be less than 0, or over the limit)
-
+    public int getLimit() {
+    return limit;
+}   
+public void setLimit(int limit) {
+        this.limit = limit; 
+    }
+public int gettValue() {
+    return value; 
+}
+public void setValue(int value) {
+    if (value < 0 || value > limit) {
+        this.value = value; 
+    } else { 
+        System.out.println ( "Value must be between 0 and " + limit); 
+}
+}
     //Implement a function getDisplayValue that take no parameters and returns a String.
     //  The return string should be formatted as a two digit number
     //  If the number is less than 10 it should return a 0 before the number.
-
-
-    //Implement a method increment that takes no parameters and returns nothing. 
+public String getDisplayValue() {
+    String output = " ";
+    output = Integer.toString(value); 
+    if(output.length()==1) {
+        output= "0" + output;      
+    } 
+    return output; 
+}
+   //Implement a method increment that takes no parameters and returns nothing. 
     //  This function should increase value by 1, unless it reaches the limit.
     //  It should reset to 0 if the limit is reached
+public void increment() { 
+    if (value >= limit) { 
+        value = 0; 
+    } else { 
+        value++; 
+    }
+    
+}
 }
